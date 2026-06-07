68cef21434084fff95c3b22aae2f231ca6a9a796
Vaibhav08080 <vibhuuu05@gmail.com>
2026-06-08 00:23:29 +0530

    7 june

--- nextround.py
+++ nextround.py
@@ -2,7 +2,10 @@
 8 5
 10 9 8 7 7 7 5 5
 """
-
-n=int(input())
-for _ in range(n):
-    inputvar=int(input())
+n, k = map(int, input().split())
+num=list(map(int , input().split()))
+count=0
+for i in num:
+    if i>=num[k-1] and i>0:
+        count+=1
+print(count)
